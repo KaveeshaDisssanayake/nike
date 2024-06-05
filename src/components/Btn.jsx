94b4c1ab506fc630bd82f4b0d0176a2d97881a8a
Kaveesha Dissanayake <90174106+KaveeshaDisssanayake@users.noreply.github.com>
2024-06-05 15:35:58 +0530

    Special offer section

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -1,8 +1,19 @@
 
 
-const Btn = ({label , iconURL}) => {
+const Btn = ({label , iconURL , backgroundColor ,textColor,borderColor}) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red  rounded-full text-white border-coral-red">
+    <button className={`flex
+     justify-center items-center gap-2
+      px-7 py-4 border font-montserrat 
+      text-lg leading-none 
+    ${
+      backgroundColor
+      ? `${backgroundColor} 
+
+      ${textColor} ${borderColor}`
+      : "bg-coral-red text-white border-coral-red"
+     } rounded-full w-full"}`}
+     >
         {label}
 
        {iconURL &&  <img 
